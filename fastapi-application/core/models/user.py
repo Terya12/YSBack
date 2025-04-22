7f4bc013703a85033bc286961170e2e6ad566243
Olexandr Tereshchenko <Terya77812@gmail.com>
2025-04-22 12:36:51 +0300

    add users model

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,5 +1,6 @@
 from typing import TYPE_CHECKING
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy import Column, String
 from core.models.base import Base
 from core.models.mixins.id_int_pk import IdIntPkMixin
 from core.types.user_id import UserIDType
@@ -9,7 +10,12 @@ if TYPE_CHECKING:
 
 
 class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[UserIDType]):
-
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
+    middle_name = Column(String, nullable=True)
+    phone_number = Column(String, nullable=True)
+    address = Column(String, nullable=True)
+    
     @classmethod
     def get_db(cls, session: "AsyncSession"):
         return SQLAlchemyUserDatabase(session, User)
