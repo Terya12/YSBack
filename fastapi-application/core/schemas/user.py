846c9d145a1bcd99941c9fa0df8c3182d051ccb4
Olexandr Tereshchenko <Terya77812@gmail.com>
2025-04-22 12:35:00 +0300

    add users schemas

--- fastapi-application/core/schemas/user.py
+++ fastapi-application/core/schemas/user.py
@@ -1,15 +1,27 @@
 
 from core.types.user_id import UserIDType
 from fastapi_users import schemas
-
+from typing import Optional
 
 class UserRead(schemas.BaseUser[UserIDType]):
-    pass
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    middle_name: Optional[str] = None
+    phone_number: Optional[str] = None
+    address: Optional[str] = None
 
 
 class UserCreate(schemas.BaseUserCreate):
-    pass
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    middle_name: Optional[str] = None
+    phone_number: Optional[str] = None
+    address: Optional[str] = None
 
 
 class UserUpdate(schemas.BaseUserUpdate):
-    pass
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    middle_name: Optional[str] = None
+    phone_number: Optional[str] = None
+    address: Optional[str] = None
